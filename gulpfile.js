d94e1ee8a2cb8d39686b87fef9000b73de0986eb
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-04 23:25:58 -0500

    Added HTML gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,24 @@ const browserSync = require('browser-sync');
 const concat = require('gulp-concat');
 
 const paths = {
-  scripts: ['./js/index.js', './js/**/*.js'],
-  styles: ['./css/styles.css', './css/*.css']
+  scripts: ['./src/js/main.js', './src/js/**/*.js'],
+  styles: ['./src/css/styles.css', './src/css/*.css'],
+  html: ['./src/html/index.html', './src/html/*.html']
 };
 
 gulp.task('serve', () => {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './build'
     }
   });
 });
 
 gulp.task('js:build', () => {
-  browserSync.reload();
   return gulp
     .src(paths.scripts)
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./js'));
+    .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('js:watch', ['js:build'], done => {
@@ -28,8 +28,18 @@ gulp.task('js:watch', ['js:build'], done => {
   done();
 });
 
+gulp.task('html:build', () => {
+  return gulp.src(paths.html).pipe(gulp.dest('./build'));
+});
+
+gulp.task('html:watch', ['html:build'], function(done) {
+  browserSync.reload();
+  done();
+});
+
 gulp.task('watch', () => {
   gulp.watch(paths.scripts, ['js:watch']);
+  gulp.watch(paths.html, ['html:watch']);
 });
 
 gulp.task('default', ['serve', 'watch']);
