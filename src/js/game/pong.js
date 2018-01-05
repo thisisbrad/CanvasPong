62325cf0234092c6f2016c5fd77ac1ef122f4579
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-05 18:19:57 -0500

    Drawing on the canvas

--- src/js/game/pong.js
+++ src/js/game/pong.js
@@ -1,1 +1,21 @@
-console.log('pong.js');
+let canvas;
+let canvasContext;
+
+window.onload = () => {
+  console.log('HOLLA');
+  canvas = document.getElementById('pong');
+  canvasContext = canvas.getContext('2d');
+  drawEverything();
+};
+
+function drawEverything() {
+  console.log('DRAWING');
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = 'red';
+  canvasContext.fillRect(100, 200, 50, 25);
+  canvasContext.fillStyle = 'green';
+  canvasContext.fillRect(140, 240, 10, 10);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(500, 400, 50, 50);
+}
